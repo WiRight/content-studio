b33026e1a1981bfcf64ef3067581488ef17f6eca
WiRight <ruslan.mahin1@gmail.com>
2025-11-13 16:22:33 +0300

    add custom component

---
+++ app/components/SignImage.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+interface Props {
+  src: string
+  sign: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <div>Image: {{ src }}</div>
+    <div>Sign: {{ sign }}</div>
+  </div>
+</template>
+
+<style scoped></style>
